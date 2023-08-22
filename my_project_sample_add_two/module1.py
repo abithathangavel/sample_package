a97a7cbb936791d3170f45aa9f4c6fc018e5eec1
abithathangavel <abithathangavel03@gmail.com>
2023-08-22 13:37:14 +0530

    Add files via upload

---
+++ my_project_sample_add_two/module1.py
@@ -0,0 +1,5 @@
+def AddTwo(num):
+    return num+2
+
+def SayHello(name):
+    return "Hello"+name+" , this is package"
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my-project-sample-add-two',
+    version='0.1',
+    description='A simple package ',
+    author='author',
+    author_email='<name>@gmail.com',
+    packages=find_packages(),
+)
